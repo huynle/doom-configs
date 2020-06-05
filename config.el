0df53f3e97f22fe0a4cebd7792cc03121c2264f0
Huy Le <dacrazyazn@gmail.com>
2020-06-05 17:08:14 -0600

    updated configurations, tweaking it to be a little closer to vim

--- config.el
+++ config.el
@@ -1,7 +1,8 @@
 (setq user-full-name "Huy Le")
 (setq user-mail-address "dacrazyazn.com")
 (setq create-lockfiles nil)
-;; (setq doom-localleader-key ";")
+(setq doom-localleader-key ";")
+(setq doom-localleader-alt-key "M-;")
 (map! :leader
       (:prefix "TAB"
         :desc "Rename workspace"       "r"  #'+workspace/rename)
@@ -13,19 +14,43 @@
         :desc "Browse mode notes"    "m" #'+brett/find-notes-for-major-mode
         :desc "Browse project notes" "p" #'+brett/find-notes-for-project)
       (:prefix "t"
-        :desc "Rainbow mode" "r" #'rainbow-mode)
-      (:prefix "/"
-        :desc "Hex colors" "h"  #'counsel-hex-colors))
+        :desc "Rainbow mode" "r" #'rainbow-mode))
 (map!
  (:after evil
    :m  "] SPC" #'evil-motion-insert-newline-below
    :m  "[ SPC" #'evil-motion-insert-newline-above))
-(map!
- (:after evil
-   :en "C-h"   #'evil-window-left
-   :en "C-j"   #'evil-window-down
-   :en "C-k"   #'evil-window-up
-   :en "C-l"   #'evil-window-right))
+;; (map!
+;;  (:after evil
+;;    :en "C-h"   #'evil-window-left
+;;    :en "C-j"   #'evil-window-down
+;;    :en "C-k"   #'evil-window-up
+;;    :en "C-l"   #'evil-window-right))
+;; Many thanks to the author of and contributors to the following posts:
+;; https://gist.github.com/mislav/5189704
+;; https://robots.thoughtbot.com/post/53022241323/seamlessly-navigate-vim-and-tmux-splits
+;;
+;; TODO: Make a script that generates tmux and emacs code without duplication
+;;
+;; NOTE: My keybindings are not the default emacs ones, using windmove
+
+;; Try to move direction, which is supplied as arg
+;; If cannot move that direction, send a tmux command to do appropriate move
+;; (defun windmove-emacs-or-tmux(dir tmux-cmd)
+;; (interactive)
+;; (if (ignore-errors (funcall (intern (concat "windmove-" dir))))
+;; nil                       ;; Moving within emacs
+;; (shell-command tmux-cmd)) ;; At edges, send command to tmux
+;; )
+
+;; ;Move between windows with custom keybindings
+;; (global-set-key (kbd "C-k")
+;;    '(lambda () (interactive) (windmove-emacs-or-tmux "up"  "tmux select-pane -U")))
+;; (global-set-key (kbd "C-j")
+;;    '(lambda () (interactive) (windmove-emacs-or-tmux "down"  "tmux select-pane -D")))
+;; (global-set-key (kbd "C-l")
+;;    '(lambda () (interactive) (windmove-emacs-or-tmux "right" "tmux select-pane -R")))
+;; (global-set-key (kbd "C-h")
+;;    '(lambda () (interactive) (windmove-emacs-or-tmux "left"  "tmux select-pane -L")))
 ;; (map! (:localleader
 ;;         (:after evil-org
 ;;           :map evil-org-mode-map
@@ -62,3 +87,41 @@
 (after! all-the-icons
   (setq all-the-icons-scale-factor 1.0))
 ;; (setq eldoc-idle-delay 0)
+(setq doom-line-numbers-style 'relative)
+(use-package! org
+  :config
+  (setq org-agenda-files (file-expand-wildcards "~/docs/org/*.org"))
+  (setq org-directory (expand-file-name "~/docs/org"))
+  (setq org-cycle-separator-lines 1)
+  (defvar +org-dir (expand-file-name "~/docs/org"))
+  (setq org-capture-templates
+        '(("c" "Code Task" entry (file+headline "~/docs/org/main.org" "Coding Tasks")
+           "* TODO %?\n  Entered on: %U - %a\n")
+          ("t" "Task" entry (file+headline "~/docs/org/main.org" "Tasks")
+           "* TODO %?\n  Entered on: %U")
+          ("n" "Note" entry (file+datetree "~/docs/org/main.org")
+           "* %?\n\n"))))
+;; (setq org-attach-directory $HOME/testing/attachment)
+(setq centaur-tabs-height 25)
+(setq centaur-tabs-cycle-scope 'tabs)
+;; (setq doom-modeline-def-modeline "project")
+
+;; (use-package doom-modeline
+;;   :ensure t
+;;   :init (doom-modeline-mode 1)
+;;   )
+;; (with-eval-after-load "doom-modeline"
+;;   (doom-modeline-def-modeline 'main
+;;   '(misc-info bar workspace-name window-number modals matches buffer-info remote-host buffer-position word-count parrot selection-info)
+;;   '(objed-state persp-name battery grip irc mu4e gnus github debug lsp minor-modes input-method indent-info buffer-encoding major-mode process vcs checker)
+;;   )
+;; )
+  ;; Define your custom doom-modeline
+;; (doom-modeline-def-modeline 'my-simple-line
+;;     '(bar " " buffer-info)
+;;     '(misc-info))
+
+;; ;; Add to `doom-modeline-mode-hook` or other hooks
+;; (defun setup-custom-doom-modeline ()
+;;     (doom-modeline-set-modeline 'my-simple-line 'default))
+;; (add-hook 'doom-modeline-mode-hook 'setup-custom-doom-modeline)
--- init.el
+++ init.el
@@ -41,7 +41,7 @@
        (popup +defaults)   ; tame sudden yet inevitable temporary windows
        ;;pretty-code       ; ligatures or substitute text with pretty symbols
        ;;tabs              ; an tab bar for Emacs
-       ;;treemacs          ; a project drawer, like neotree but cooler
+       treemacs          ; a project drawer, like neotree but cooler
        ;;unicode           ; extended unicode support for various languages
        vc-gutter         ; vcs diff in the fringe
        vi-tilde-fringe   ; fringe tildes to mark beyond EOB
@@ -140,7 +140,10 @@
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       (org +dragndrop)              ; organize your plain life in plain text
+       (org +dragndrop
+            +attach
+            +babel
+            +capture)              ; organize your plain life in plain text
        ;;perl              ; write code no one else can comprehend
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
